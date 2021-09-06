970ce7eb8f14ec035ea9e60db8337089758a6253
Suleiman Mai-Abba <suleyman.abba@gmail.com>
2021-09-06 21:09:14 +0100

    Added shell context processor

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from portal import create_app
+from portal import db
+from portal.student.models import Student
 
 
 app = create_app('dev')
+
+@app.shell_context_processor
+def context_processor():
+    return dict(db = db, Student = Student)
 
 if __name__ == "__main__":
     app.run()
