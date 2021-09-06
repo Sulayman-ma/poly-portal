5b957ca360c3b4ad17b51fcbb6d548c2a7310a6b
Suleiman Mai-Abba <suleyman.abba@gmail.com>
2021-09-06 21:09:40 +0100

    Set database URI to localhost:3306

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ base_dir = path.abspath(path.dirname(__file__))
 class Config:
     SECRET_KEY = 'another random string'
     WTF_CSRF_SECRET_KEY = 'a random string'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
@@ -15,6 +16,8 @@ class Config:
 
 class Development(Config):
     DEBUG = 1
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:illyrian00@localhost:3306/poly_portal'
+
 
 
 configs = {
